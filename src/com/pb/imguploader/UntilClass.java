101f4fcf3a76ceb940b266aec43312de48edb7ef
topiniu <topiniu@outlook.com>
2017-07-04 16:04:16 +0800

    Base on servlet3.0

---
+++ src/com/pb/imguploader/UntilClass.java
@@ -0,0 +1,101 @@
+package com.pb.imguploader;
+import java.io.*;
+import java.util.*;
+import java.awt.image.*;
+
+import javax.imageio.*;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.Color;
+
+
+public class UntilClass {
+    
+    public static void main(String[] args) throws Exception {
+//        File input = new File("t.png");
+        
+        
+    }
+    public String saveImg(File input) throws Exception{
+    	System.out.println(">>file name  " + input.getName());
+        
+        if(input.getName().endsWith("png"))
+        {
+            BufferedImage bufferedImage = ImageIO.read(input);
+            
+            BufferedImage newBufferedImage = new BufferedImage(bufferedImage.getWidth(),bufferedImage.getHeight(),BufferedImage.TYPE_INT_RGB);
+            newBufferedImage.createGraphics().drawImage(bufferedImage,0,0,Color.WHITE,null);
+            File newJpg = new File(input.getName().split("\\.")[0] + ".jpg");
+            ImageIO.write(newBufferedImage,"jpg",newJpg);
+            
+            
+            
+            return new UntilClass().compress(newJpg,newJpg.getName(),input.length());
+        }else{
+            return new UntilClass().compress(input,null,input.length());
+        }
+    }
+    
+    private String compress(File input,String newJpgName,long fullSize) throws Exception{
+        if(input == null)
+            return null;
+        
+//        double inputSize = input.length();
+
+		Properties prop = new Properties();
+		ClassLoader loader = this.getClass().getClassLoader();
+		
+		prop.load(loader.getResourceAsStream("default.properties"));
+		
+        
+        InputStream is = new FileInputStream(input);
+        
+        BufferedImage image = ImageIO.read(is);
+        
+        File compressedImageFile = new File(prop.getProperty("_COMPRESSED_UPLOADIMGFOLDER") + input.getName().split("\\.")[0]+".jpg");
+        
+        if(!compressedImageFile.exists()){
+        	compressedImageFile.getParentFile().mkdirs();
+        	System.out.println(">> Create dir successed");
+        }
+        
+        OutputStream os =new FileOutputStream(compressedImageFile);
+        
+        ImageWriter jpgWriter =  (ImageWriter) ImageIO.getImageWritersByFormatName("jpg").next();
+        
+        
+        ImageWriteParam jpgWriteParam = jpgWriter.getDefaultWriteParam();
+        
+        ImageOutputStream ios = ImageIO.createImageOutputStream(os);
+        
+        
+        
+        jpgWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        jpgWriteParam.setCompressionQuality(Float.parseFloat(prop.getProperty("_COMPRESS_LEVEL")));
+        
+        
+        jpgWriter.setOutput(ios);
+        
+        
+        
+        jpgWriter.write(null, new IIOImage(image, null, null), jpgWriteParam);
+        
+        System.out.println("COMPRESSED length = " + compressedImageFile.length());
+        System.out.println("full size = " + fullSize);
+        System.out.println(compressedImageFile.length() > fullSize);
+        
+//        if(compressedImageFile.length() > fullSize){
+//        	System.out.println(">>> Go to re compress");
+//        	compress(compressedImageFile, newJpgName, fullSize);
+//        }
+        
+        if(newJpgName != null)
+        	new File(newJpgName).delete();
+        
+        System.out.println(">>>Saved");
+        
+        os.close();
+        ios.close();
+        
+        return compressedImageFile.getAbsolutePath();
+    }
+}
---
+++ src/com/pb/imguploader/UploadImg.java
@@ -0,0 +1,103 @@
+package com.pb.imguploader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import java.util.Properties;
+
+
+/**
+ * Servlet implementation class UploadImg
+ */
+@WebServlet("/uploadimg")
+@MultipartConfig
+public class UploadImg extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UploadImg() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Fang wen cheng gong");
+
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setCharacterEncoding("UTF-8");
+
+		Properties prop = new Properties();
+		ClassLoader loader = this.getClass().getClassLoader();
+		
+		prop.load(loader.getResourceAsStream("default.properties"));
+		
+		
+		
+		String description = request.getParameter("description");
+		Part filePart = request.getPart("file");
+		String fileName = getSubmittedFileName(filePart);
+		
+		InputStream inputStream = filePart.getInputStream();
+		
+		File uploads = new File(prop.getProperty("_FULL_UPLOADIMGFOLDER"));
+		System.out.println(prop.getProperty("_FULL_UPLOADIMGFOLDER"));
+		System.out.println(fileName);
+		
+		File file = new File(uploads,fileName);
+		
+		if(!file.exists()){
+			file.getParentFile().mkdirs();
+//			file.createNewFile();
+		}
+		
+		InputStream input = inputStream;
+		try{
+			Files.copy(input, file.toPath());
+		}catch(FileAlreadyExistsException e){
+			file.delete();
+			Files.copy(input, file.toPath());
+			System.out.println(">>> Re creare file");
+		}
+		
+		UntilClass uc = new UntilClass();
+		try {
+			uc.saveImg(file);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	private static String getSubmittedFileName(Part part) {
+	    for (String cd : part.getHeader("content-disposition").split(";")) {
+	        if (cd.trim().startsWith("filename")) {
+	            String fileName = cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
+	            return fileName.substring(fileName.lastIndexOf('/') + 1).substring(fileName.lastIndexOf('\\') + 1); // MSIE fix.
+	        }
+	    }
+	    return null;
+	}
+}
